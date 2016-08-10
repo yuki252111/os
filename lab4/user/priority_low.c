5372089a1ffe4bcd9d30499f17d272b10e586e17
Your Name <you@example.com>
2016-08-10 17:56:36 +0800

    os lab

---
+++ lab4/user/priority_low.c
@@ -0,0 +1,11 @@
+#include <inc/x86.h>
+#include <inc/lib.h>
+
+void
+umain(int argc, char **argv)
+{
+	uint32_t i=0;
+	for(i=0;i<3;i++)
+	cprintf("[%8x] Priority Low is Running!\n",sys_getenvid());
+	return;
+}
